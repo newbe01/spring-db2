b59e869a5d15c5394a0b8cce65a7e31de35b8d65
newbe01 <pkwaa3@gmail.com>
2023-12-19 21:48:25 +0900

    spring db - testase

--- src/test/java/itemservice/db/repository/ItemRepositoryTest.java
+++ src/test/java/itemservice/db/repository/ItemRepositoryTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 
