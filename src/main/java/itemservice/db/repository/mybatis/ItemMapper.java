f4b000d6126b862755bd458faf997931a8e566a9
newbe01 <pkwaa3@gmail.com>
2023-12-20 00:19:26 +0900

    spring db - mybatis
    annotation을 사용해서 쿼리를 할 수 있다.

--- src/main/java/itemservice/db/repository/mybatis/ItemMapper.java
+++ src/main/java/itemservice/db/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import itemservice.db.repository.ItemSearchCond;
 import itemservice.db.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public interface ItemMapper {
     // @Param 은 parameter 가 2개 이상일때
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
 
+    @Select("select id, item_name, price, quantity from item where id = #{id}")
     Optional<Item> findById(Long id);
 
     List<Item> findAll(ItemSearchCond itemSearch);
